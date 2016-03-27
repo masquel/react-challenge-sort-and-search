90b77328a6d1959d2601959bef4b0a10bff75ae3
Alexey <grimzest@gmail.com>
2016-03-27 21:20:46 +0300

    Update 2016-03-27T18:20:43.703Z

--- gulp/watch.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = (gulp, plugins, config) => () => {
-  gulp.watch(config.paths.stylus, gulp.series('build-stylus'));
-  gulp.watch(config.paths.js, gulp.series('build-js'));
-  gulp.watch(config.paths.html, gulp.series('copy'));
-};
